5504fcc104652f9d53f41908d3e3f7350b7aa1b1
unknown <kuroneko7975@gmail.com>
2025-11-08 19:54:21 +0700

    Tambah fitur INSERT data mahasiswa ke database

--- latihan_branch/tambahdata.php
+++ latihan_branch/tambahdata.php
@@ -1,10 +1,13 @@
 <?php
-include 'koneksi.php';
+include 'koneksi.php'; // hubungkan ke file koneksi
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $nama = $_POST['nama'];
-    $sql = "INSERT INTO mahasiswa (nama) VALUES ('$nama')";
-    if(mysqli_query($conn, $sql)){
+    $nim = $_POST['nim'];
+    $jurusan = $_POST['jurusan'];
+
+    $sql = "INSERT INTO mahasiswa (nama, nim, jurusan) VALUES ('$nama', '$nim', '$jurusan')";
+    if (mysqli_query($conn, $sql)) {
         echo "Data berhasil ditambahkan!";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
@@ -12,8 +15,25 @@ if(isset($_POST['submit'])){
 }
 ?>
 
-<form method="POST">
-    <input type="text" name="nama" placeholder="Masukkan nama">
-    <button type="submit" name="submit">Tambah</button>
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    <h2>Form Tambah Data</h2>
+    <form method="POST" action="">
+        <label>Nama:</label><br>
+        <input type="text" name="nama" required><br><br>
+
+        <label>NIM:</label><br>
+        <input type="text" name="nim" required><br><br>
+
+        <label>Jurusan:</label><br>
+        <input type="text" name="jurusan" required><br><br>
+
+        <button type="submit" name="submit">Tambah</button>
+    </form>
+</body>
+</html>
 
